413bcee06217cbbfc4e6e0b2975aba9ca2c3a003
Prism <ershubhcsofficial@gmail.com>
2025-03-19 17:27:21 +0530

    added condition check function

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -3,13 +3,24 @@
 #include<iostream>
 using namespace std;
 
+// function to check board results
+int checkBoard(char board[3][3]) {
+
+}
+
 // function to take input in tic tac toe
-int takeInput(char board[3][3]) {
+int takeInput(char board[3][3], int turns) {
    int row, col;
    cout << "Enter row (0-2): ";
    cin >> row;
    cout << "Enter column (0-2): ";
    cin >> col;
+
+   // condition check to win or draw
+   if(turns<4) {
+      checkBoard(board);
+   }
+   
    return 0;
 }
 
@@ -18,7 +29,8 @@ int main() {
    int turns = 9;
 
    for( int i=1; i<=9; i++ ) {
-      takeInput(board);
+      turns++;
+      takeInput(board, turns);
    }
 
    return 0;
