0d64e3f74515742dc8b26fc4de3927b867650f62
Prism <ershubhcsofficial@gmail.com>
2025-03-21 19:44:59 +0530

    Minor changes : added func declarations to avoid conflicts

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -3,10 +3,56 @@
 #include<iostream>
 using namespace std;
 
+// function declarations
+void displayBoard(char board[3][3]);
+void fillBoard(char board[3][3]);
+void toPlay(char board[3][3], int turns);
+int checkBoard(char board[3][3]);
+int takeInput(char board[3][3], int turns);
+
+
+int main() {
+   // declare a 3x3 matrix for the game 
+   char board[3][3];
+
+   // variable to count number of turns
+   int turns = 1;
+
+   // fucntion call to fill the board with empty spaces
+   fillBoard(board);
+
+   // function call to start the game
+   toPlay(board, turns);
+
+   return 0;
+}
+
+
+// function to fill the board with space
+void fillBoard(char board[3][3]) {
+   for(int i=0; i<3; i++) {
+      for(int j=0; j<3; j++) {
+         board[i][j]=' ';
+      }
+   }
+}
+
+
+// function to start the game 
+void toPlay(char board[3][3], int turns) {
+   // loop to take input in the board
+   for( int i=1; i<=9; i++ ) {
+      takeInput(board, turns);
+
+      // fucntion call to display function
+      displayBoard(board);
+      turns++;
+   }
+}
+
+
 // display the board
 void displayBoard(char board[3][3]) {
-   // cout<<endl;
-
    for(int i=0; i<3; i++) {
       for(int j=0; j<3; j++) {
          cout<<board[i][j]<<" ";
@@ -15,11 +61,6 @@ void displayBoard(char board[3][3]) {
    }
 }
 
-// function to check board results
-int checkBoard(char board[3][3]) {
-
-   return 0;
-}
 
 // function to take input in the board
 int takeInput(char board[3][3], int turns) {
@@ -35,54 +76,9 @@ int takeInput(char board[3][3], int turns) {
    }
 
    // function call to check the board status
-   if(turns<4) {
-      
-      // to check
-      cout<<"Yes";
-      checkBoard(board);
-   }
-
-   return 0;
-}
-
-// function to play the game 
-void toPlay(char board[3][3], int turns) {
-   // loop to take input in the board
-   for( int i=1; i<9; i++ ) {
-      takeInput(board, turns);
-
-      // fucntion call to display function
-      displayBoard(board);
-      turns++;
-   }
-}
-
-// function to fill the board with space
-void fillBoard(char board[3][3]) {
-   for(int i=0; i<3; i++) {
-      for(int j=0; j<3; j++) {
-         board[i][j]=' ';
-      }
-   }
-}
-
-int main() {
-   // declare a matrix 3x3 for the game 
-   char board[3][3];
-   int turns = 1; // count the number of turns
-
-   // fucntion to fill the board with empty spaces
-   fillBoard(board);
-
-   //function to play the game
-   toPlay(board, turns);
-
-   for( int i=1; i<=9; i++ ) {
-      turns++;
-      takeInput(board, turns);
-      // display function
-      displayBoard(board);
-   }
+   // if(turns<4) {
+   //    checkBoard(board);
+   // }
 
    return 0;
 }
