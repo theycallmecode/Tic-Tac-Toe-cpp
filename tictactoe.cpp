50c82697f442f631ca3e654d61aa0bbb970a860d
Prism <ershubhcsofficial@gmail.com>
2025-03-21 20:51:36 +0530

    Update : Added rows check feature

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -6,23 +6,29 @@ using namespace std;
 // function declarations
 void displayBoard(char board[3][3]);
 void fillBoard(char board[3][3]);
-void toPlay(char board[3][3], int turns);
-int checkBoard(char board[3][3]);
-int takeInput(char board[3][3], int turns);
+void toPlay(char board[3][3]);
+int takeInput(char board[3][3]);
+void checkBoard(char board[3][3]);
+
+
+// global variables
+int turns = 1; // keeps track of turns
+bool game = false; // game status
+int i=0, j=0; // loop counters
 
 
 int main() {
    // declare a 3x3 matrix for the game 
    char board[3][3];
 
-   // variable to count number of turns
-   int turns = 1;
-
    // fucntion call to fill the board with empty spaces
    fillBoard(board);
 
    // function call to start the game
-   toPlay(board, turns);
+   toPlay(board);
+
+   if(game) cout<<endl<<"Player WINS";
+   else cout<<endl<<"DRAW";
 
    return 0;
 }
@@ -39,15 +45,37 @@ void fillBoard(char board[3][3]) {
 
 
 // function to start the game 
-void toPlay(char board[3][3], int turns) {
+void toPlay(char board[3][3]) {
    // loop to take input in the board
    for( int i=1; i<=9; i++ ) {
-      takeInput(board, turns);
+      takeInput(board);
 
-      // fucntion call to display function
+      // fucntion call to display the game board
       displayBoard(board);
       turns++;
    }
+}
+
+
+// function to take input in the board
+int takeInput(char board[3][3]) {
+   int row, col;
+   cout << "Enter row & column (0-2): ";
+   cin >> row >> col;
+
+   if(turns%2==0) { // player 2 turn
+      board[row][col]='X';
+   }
+   else { // player 1 turn
+      board[row][col]='O';
+   }
+
+   // function call to check the board status
+   if(turns>4) {
+      checkBoard(board);
+   }
+
+   return 0;
 }
 
 
@@ -62,23 +90,23 @@ void displayBoard(char board[3][3]) {
 }
 
 
-// function to take input in the board
-int takeInput(char board[3][3], int turns) {
-   int row, col;
-   cout << "Enter row & column (0-2): ";
-   cin >> row >> col;
+// function to check the board status
+void checkBoard(char a[3][3]) {
 
-   if(turns%2==0) { // player 2 turn
-      board[row][col]='X';
+   // check rows
+   i=0,j=0; // reset loop counters
+   while(i<2) {
+      while(j<1) {
+         if ( a[i][j] == ' ' ) {
+            continue;
+         }
+         else if(a[i][j]==a[i][j+1] && a[i][j]==a[i][j+2]) {
+            game = true;
+            return ;
+         }
+         j++;
+      }
+      i++;
    }
-   else { // player 1 turn
-      board[row][col]='O';
-   }
-
-   // function call to check the board status
-   // if(turns<4) {
-   //    checkBoard(board);
-   // }
-
-   return 0;
+   
 }
