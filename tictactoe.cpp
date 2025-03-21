969bb713e5bd61f0cd6d4a363b4634d23a229958
Prism <ershubhcsofficial@gmail.com>
2025-03-21 21:08:11 +0530

    Update : Added coloumnss check feature

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -108,5 +108,22 @@ void checkBoard(char a[3][3]) {
       }
       i++;
    }
+
+   // check columns
+   i=0,j=0; // reset loop counters
+   while(i<2) {
+      while(j<1) {
+         if ( a[i][j] == ' ' ) {
+            continue;
+         }
+         else if(a[i][j]==a[i+1][j] && a[i][j]==a[i+2][j]) {
+            game = true;
+            return ;
+         }
+         j++;
+      }
+      i++;
+   }
+
    
 }
